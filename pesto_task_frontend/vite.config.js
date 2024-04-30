f51055bf7916ff390684e9d670f9ebef581a41ad
Rahul Solanki <99829859+solankiRahulFr@users.noreply.github.com>
2024-04-30 13:52:51 +0200

    Update vite.config.js

--- pesto_task_frontend/vite.config.js
+++ pesto_task_frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://pesto-taskapp.onrender.com',
         changeOrigin: true,
       },
     }
